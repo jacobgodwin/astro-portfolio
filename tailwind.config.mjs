21f143aa0f1fb4ff9dfb5891d6f9bddf41735136
Jacob Godwin <jpg174@gmail.com>
2025-02-10 14:32:27 -0500

    update font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {
-      fontFamily: {
-        prime: 'Courier Prime, monospace',
-      },
+    fontFamily: {
+      // https://github.com/system-fonts/modern-font-stacks
+      oldstyle: [
+        'Iowan Old Style',
+        'Palatino Linotype',
+        'URW Palladio L',
+        'P052',
+        'serif',
+      ],
     },
   },
   plugins: [],

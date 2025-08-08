37b2366c1fc122e5d7def36a483368ff94e67900
Jacob Godwin <jpg174@gmail.com>
2025-08-08 14:38:38 -0400

    update darkMode value

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ['media'],
+  darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     container: {
